3b5dbc3d782c329b47b65e84f18153f522f76218
anand <anandsumid@gmail.com>
2024-01-08 22:22:47 +0100

    added mongoose

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
+
 
 app.get('/', (req,res) =>{
     res.send("Hello NODE API")
 })
-app.listen(3000, ()=>{
-    console.log("Node API app is running on PORT 3000")
+app.get('/blog', (req,res)=>{
+    res.send("Hello blog")
+})
+
+
+mongoose.connect('mongodb+srv://anandsumid:Bibnsgejl1@cluster0.a8lbdrn.mongodb.net/?retryWrites=true&w=majority')
+.then(()=>{
+    console.log("connected to MONGODB")
+    app.listen(3000, ()=>{
+        console.log("Node API app is running on PORT 3000")
+    })
+}).catch(()=>{
+    console.log(error)
 })
